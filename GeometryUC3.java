5ac188557d2e7cdcd3f0b3aae3f4a0c3bf0ecd3b
Deepak Jaiswal <jaiswaldj96@gmail.com>
2021-03-25 21:34:27 +0530

    [Deepak] added GeometryUC3.java

---
+++ GeometryUC3.java
@@ -0,0 +1,14 @@
+public class GeometryUC3 {
+	
+
+	public static void main(String args[]) {
+		String str1 = "Equal"; 
+		
+		String str2 = "Greater";
+		String str3 = "lesser";
+			
+		System.out.println(str1.compareTo(str2));
+
+		System.out.println(str2.compareTo(str3));
+	   }
+	 }    
